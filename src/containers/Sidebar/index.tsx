f18bf3253151f5b867286ca7e686eaf673b92048
Octavio Fernandes Pereira <octaviofp2003@outlook.com>
2024-01-23 16:46:26 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SideBarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/Tavimfernandes.png" />
-    <Titulo fontSize={20}>TavimFernandes</Titulo>
+    <SideBarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Octávio Fernandes</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        @TavimFernandes
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-End
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SideBarContainer>
   </aside>
 )
 
